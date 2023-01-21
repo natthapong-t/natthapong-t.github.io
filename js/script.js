ff3f54acee6b761b7699bca62da8578b81adddf6
Natthapong <natthapong.t@kkumail.com>
2023-01-21 16:53:55 +0700

    add them back

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// var progress = document.getElementById('progressbar');
+// var totalHeight = document.body.scrollHeight - window.innerHeight;
+// window.onscroll = function(){
+//     var progressHeight = (window.pageYOffset / totalHeight) * 100;
+//     progress.style.height = progressHeight + "%";
+// }
+
+
