25aa158abc84e55442b25a6619c74db683768bba
Natthapong <natthapong.t@kkumail.com>
2023-02-10 17:53:24 +0700

    deleted to update the new one

--- js/script.js
+++
@@ -1,8 +0,0 @@
-// var progress = document.getElementById('progressbar');
-// var totalHeight = document.body.scrollHeight - window.innerHeight;
-// window.onscroll = function(){
-//     var progressHeight = (window.pageYOffset / totalHeight) * 100;
-//     progress.style.height = progressHeight + "%";
-// }
-
-
